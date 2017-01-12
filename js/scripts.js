6fda101e86af09098f9f0f37f1a59fbe546cbd32
Caitlin Hines and Kaz Matthews <kaz@>
2017-01-12 11:40:18 -0800

    add variables for quiz input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $("form#preferences").submit(function(event) {
     event.preventDefault();
+    var userAge = parseInt($("#age").val());
 
+    var userGenderPreference = $("input:checked").val();
+        debugger;
   });
 });
