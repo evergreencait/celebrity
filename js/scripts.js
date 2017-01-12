5c7c09456891481bea987d6e1aade43a0e9a5525
Caitlin Hines and Kaz Matthews <kaz@>
2017-01-12 11:51:47 -0800

    add if else skeleton.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,23 @@ $(document).ready(function() {
   $("form#preferences").submit(function(event) {
     event.preventDefault();
     var userAge = parseInt($("#age").val());
-
     var userGenderPreference = $("input:checked").val();
-        debugger;
+
+    if (userAge && userGenderPreference) {
+
+      if (userAge > 30 && userGenderPreference === "female") {
+        $(".meryl").show();
+      } else if () {
+
+      } else if () {
+
+      } else {
+
+      }
+
+    } else {
+      alert("not good");
+    }
+
   });
 });
